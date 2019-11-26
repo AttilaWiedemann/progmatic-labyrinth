00e157612a9025ddac2334ec783b67301341b97d
AttilaWiedemann <attila.wiedemann@gmail.com>
2019-11-26 12:56:19 +0100

    implements RandomPlayer, passes test

--- src/main/java/com/progmatic/labyrinthproject/RandomPlayer.java
+++ src/main/java/com/progmatic/labyrinthproject/RandomPlayer.java
@@ -4,27 +4,16 @@ import com.progmatic.labyrinthproject.enums.Direction;
 import com.progmatic.labyrinthproject.interfaces.Labyrinth;
 import com.progmatic.labyrinthproject.interfaces.Player;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Random;
 
 public class RandomPlayer implements Player {
 
     @Override
     public Direction nextMove(Labyrinth l) {
-        /*
         Random r = new Random();
-        switch (r.nextInt(4)) {
-            case 0:
-                if(l.ge)
-                return Direction.NORTH;
-            case 1:
-                return Direction.SOUTH;
-            case 2:
-                return Direction.WEST;
-            default:
-                return Direction.EAST;
-        }
-
-         */
-        return null;
+        List<Direction> myArray = l.possibleMoves();
+        return myArray.get(r.nextInt(myArray.size()));
     }
 }
